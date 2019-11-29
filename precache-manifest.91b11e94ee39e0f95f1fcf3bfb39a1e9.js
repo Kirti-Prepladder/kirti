d89222f077ea882bd9c41386929c3902429405a5
Kirti-Prepladder <58203487+Kirti-Prepladder@users.noreply.github.com>
2019-11-29 03:11:56 -0800

    Add files via upload

---
+++ precache-manifest.91b11e94ee39e0f95f1fcf3bfb39a1e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624bbf483851d5f7b2da6cb2d487499e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bfa6ae58cf05769b800",
+    "url": "/static/css/main.fbf5e31f.chunk.css"
+  },
+  {
+    "revision": "311ed31d0f9714217a35",
+    "url": "/static/js/2.4e76d08d.chunk.js"
+  },
+  {
+    "revision": "5bfa6ae58cf05769b800",
+    "url": "/static/js/main.2f8000c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "790045f97f2d40382c8eecaaa38d28bf",
+    "url": "/static/media/book.790045f9.jpg"
+  },
+  {
+    "revision": "17d70611de469bc96d02db6f03c11946",
+    "url": "/static/media/slider-banner.17d70611.png"
+  }
+]);
